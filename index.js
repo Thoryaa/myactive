bfe0aa9480e01a3122c7b58b6217c160395815b8
Thoryaa <93941131+Thoryaa@users.noreply.github.com>
2022-03-15 21:27:02 +0300

    first virsion

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 app.use(express.static('assests'));
 //to support ejs file
 app.set("view engine", "ejs");
+const port = process.env.PORT || 3000;
 //to open and connect in server
-app.listen(3000, () => { console.log("you are on the server^_^"); })
+app.listen(port, () => { console.log("you are on the server^_^"); })
 app.get("/", (req, res) => {
     res.render("index")
 })
